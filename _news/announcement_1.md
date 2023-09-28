3810870f8cab8775cf59f779110c4d9979b4ae3f
abrilcf <abrilcf@users.noreply.github.com>
2023-09-28 13:21:28 +0000

    Deploying to gh-pages from @ abrilcf/abrilcf.github.io@42341ca9137fc996838ad7133f8ccf69051188f3 🚀

--- _news/announcement_1.md
+++
@@ -1,8 +0,0 @@
----
-layout: post
-date: 2023-07-17
-inline: true
-related_posts: false
----
-
-Our [paper](/publications/CodeDiff3D/index.html) got accepted at ICCV 2023! :tada:
--- publications/CodeDiff3D/static/js/gif_grid.js
+++ publications/CodeDiff3D/static/js/gif_grid.js
@@ -1,16 +1,1 @@
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  gutter: '.gutter-sizer',
-  percentPosition: true  
-});
-
-$('#image-size').click(function(){
-   if (this.checked) {
-    $( ".randomize" ).addClass( "grid-item-width2" );
-    $('.grid').masonry('layout');
-} else {
-    $( ".randomize").removeClass( "grid-item-width2" );
-    $('.grid').masonry('layout');
-}
-});
+$(".grid").masonry({itemSelector:".grid-item",columnWidth:".grid-sizer",gutter:".gutter-sizer",percentPosition:!0}),$("#image-size").click(function(){this.checked?($(".randomize").addClass("grid-item-width2"),$(".grid").masonry("layout")):($(".randomize").removeClass("grid-item-width2"),$(".grid").masonry("layout"))});
